d217d2cae7f656d35d49f0ac63f58b12e2cc6c9d
Brandon <brandonzion.sun@gmail.com>
2019-08-22 18:40:08 -0400

    save and load version 1

--- app/src/main/java/com/example/pageturner/MainActivity.java
+++ app/src/main/java/com/example/pageturner/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.pageturner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.os.Bundle;
 
@@ -11,14 +15,20 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 import static android.widget.Toast.LENGTH_LONG;
@@ -27,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     static ArrayList<Drawable> images = new ArrayList<>();
     ImageButton buttonClick;
     int CAMERA_PIC_REQUEST = 1337;
+    static ArrayList<Bitmap> files = new ArrayList<>();
+    int numberOfSongs;
+    Bitmap[] newFiles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,10 +60,12 @@ public class MainActivity extends AppCompatActivity {
 
         if(requestCode==CAMERA_PIC_REQUEST){
             Bitmap thumbnail = (Bitmap)data.getExtras().get("data");
+            files.add(thumbnail);
             Drawable drawable = new BitmapDrawable(getResources(), thumbnail);
             ImageView image = (ImageView)findViewById(R.id.imageView);
             image.setImageDrawable(drawable);
             images.add(drawable);
+
         }
         else{
             Toast.makeText(MainActivity.this,"Picture not taken", LENGTH_LONG);
@@ -62,6 +77,31 @@ public class MainActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                newFiles = files.toArray(new Bitmap[files.size()]);
+                ContextWrapper cw = new ContextWrapper(getApplicationContext());
+                File directory = cw.getDir("songs", Context.MODE_PRIVATE);
+                SharedPreferences load = getSharedPreferences("numberSongs",0);
+                int savedNumberOfSongs = load.getInt("numberOfSongs",1);
+                numberOfSongs = savedNumberOfSongs;
+
+                for(int i=0;i<newFiles.length;i++){
+                    File file = new File(directory,"page_"+savedNumberOfSongs+"-"+(i+1)+".jpg");
+                    if(!file.exists()){
+                        Log.d("path", file.toString());
+                        FileOutputStream fos = null;
+                        try {
+                            fos = new FileOutputStream(file);
+                            newFiles[i].compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                            fos.flush();
+                            fos.close();
+                        } catch (java.io.IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                numberOfSongs+=1;
+                SharedPreferences save = getSharedPreferences("numberSongs",0);
+                save.edit().putInt("numberOfSongs",numberOfSongs).commit();
                 startActivity(new Intent(MainActivity.this,pictures.class));
             }
         });
--- app/src/main/java/com/example/pageturner/pictures.java
+++ app/src/main/java/com/example/pageturner/pictures.java
@@ -1,17 +1,28 @@
 package com.example.pageturner;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class pictures extends AppCompatActivity {
@@ -20,7 +31,9 @@ public class pictures extends AppCompatActivity {
     ListView lv;
     static Drawable[] newImages;
     Toast toast;
-
+    ArrayList<Drawable> pages = new ArrayList<>();
+    Drawable[] newPages;
+    ArrayList<String> fileNames = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +47,32 @@ public class pictures extends AppCompatActivity {
             imageNames[i] = String.format("page %s",i+1);
         }
         newImages = images.toArray(new Drawable[images.size()]);
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        File directory = cw.getDir("songs", Context.MODE_PRIVATE);
+        try {
+            File[] files = new File("/data/data/com.example.pageturner/app_songs").listFiles();
+            for(int i=0;i<files.length;i++){
+                if(files[i].getName().substring(0,6).equals("page_1")){
+                    fileNames.add(files[i].getName());
+                }
+            }
+            String[] newFileNames = new String[fileNames.size()];
+            newFileNames = fileNames.toArray(newFileNames);
+            for(int i=0;i<fileNames.size();i++){
+                File f=new File(directory, newFileNames[i]);
+                Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+                pages.add(new BitmapDrawable(getResources(),b));
+
+            }
+            newPages = new Drawable[pages.size()];
+            newPages = pages.toArray(newPages);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
         lv = (ListView)findViewById(R.id.listView);
-        lv.setAdapter(new CustomAdapter(this,imageNames,newImages));
+        lv.setAdapter(new CustomAdapter(this,imageNames,newPages));
     }
     private void configureBackButton(){
         ImageButton backButton = (ImageButton) findViewById(R.id.home);
